703c3dad4485119d7f0d92dcfd7b9116e549b4e7
IvanMonjardin <71083961+IvanMonjardin@users.noreply.github.com>
2022-06-01 20:44:58 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,17 +4,21 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.26.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
   }
-  
+  required_version = ">= 1.1.0"
+
   cloud {
-    organization = "agustinavevo"
+    organization = "REPLACE_ME"
 
     workspaces {
       name = "gh-actions-demo"
     }
   }
 }
-
 
 provider "aws" {
   region = "us-east-1"
